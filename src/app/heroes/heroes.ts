8653017ace8cae1dd9f59d4d74cd3b5cb3c5ec65
Yahavba <yahav.bardavid@gmail.com>
2026-02-04 20:19:38 +0200

    implement two-way binding for hero name

--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.html',
   styleUrl: './heroes.css',
 })
