c3cf888206441ac4a90f8a37a7e5ea88a20d7ec4
Yahavba <yahav.bardavid@gmail.com>
2026-02-05 17:22:48 +0200

    add Dashboard component and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Heroes } from './heroes/heroes';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
     path: 'heroes',
     component: Heroes,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
   },
 ];
