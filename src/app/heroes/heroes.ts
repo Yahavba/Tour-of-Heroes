9ddf8c52eb57490260d357811f0c704b17005461
Yahavba <yahav.bardavid@gmail.com>
2026-02-06 01:21:28 +0200

    add functionality to delete heroes

--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -30,4 +30,9 @@ export class Heroes implements OnInit {
       this.heroes.push(hero);
     });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
