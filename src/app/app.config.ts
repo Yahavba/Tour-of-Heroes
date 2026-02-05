734af3c444dc6137a861a4f47251c356478c19a7
Yahavba <yahav.bardavid@gmail.com>
2026-02-05 22:45:15 +0200

    setup in-memory-web-api and configure HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { routes } from './app.routes';
+import { InMemoryData } from './in-memory-data';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryData, {
+        dataEncapsulation: false,
+      }),
+    ),
+  ],
 };
